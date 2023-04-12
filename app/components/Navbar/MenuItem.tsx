3a432a229c702b706d557cb78d2265b3012968ae
abr-ya <gamer05@list.ru>
2023-04-12 07:01:09 +0300

    feature: Add UserMenu component

---
+++ app/components/Navbar/MenuItem.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { FC } from "react";
+
+interface IMenuItem {
+  onClick?: () => void; // todo: сделать обязательным!
+  label: string;
+}
+
+const MenuItem: FC<IMenuItem> = ({ label, onClick }) => (
+  <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+    {label}
+  </div>
+);
+
+export default MenuItem;
