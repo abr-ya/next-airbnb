412429eb5f5bc7585bcc2b97f33bc32a6933b46c
abr-ya <gamer05@list.ru>
2023-04-14 08:09:26 +0300

    fix: Update PrismaClient instantiating

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,10 +1,16 @@
+// info: https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices#solution
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  });
 
-export default client;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
