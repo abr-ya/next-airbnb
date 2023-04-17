4655e11d97d6f89c3e6ae009edc9f9f2df8fb909
abr-ya <gamer05@list.ru>
2023-04-17 15:03:03 +0300

    feature: Add Listing Head component

--- app/components/listings/ListingClient.tsx
+++ app/components/listings/ListingClient.tsx
@@ -1,6 +1,8 @@
 import { FC, useMemo } from "react";
 import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import categories from "@/app/data/categories";
+import Container from "../Container";
+import ListingHead from "./ListingHead";
 
 interface IListingClient {
   reservations?: SafeReservation[];
@@ -10,15 +12,31 @@ interface IListingClient {
   currentUser?: SafeUser | null;
 }
 
-const ListingClient: FC<IListingClient> = ({ listing }) => {
+const ListingClient: FC<IListingClient> = ({ listing, reservations = [], currentUser }) => {
   const category = useMemo(() => {
     return categories.find((items) => items.label === listing.category);
   }, [listing.category]);
 
+  console.log(reservations, category);
+
   return (
-    <div>
-      {listing.title} - {category?.label}
-    </div>
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            todo: ListingInfo
+            <div className="order-first mb-10 md:order-last md:col-span-3">todo: ListingReservation</div>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 };
 
